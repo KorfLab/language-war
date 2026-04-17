a517a66ebd88310e2fb066ebc69dca999cc761b3
Shanoa Ice <25174488+shanoaice@users.noreply.github.com>
2026-04-16 17:22:30 -0700

    chore(script): also run kmer with anti counting

--- 00python-start-here/run.sh
+++ 00python-start-here/run.sh
@@ -4,6 +4,7 @@ JSON=../data/hmm.json
 
 python3 dust.py $FASTA > dust.out
 python3 kmers.py $FASTA 5 > kmers.out
+python3 kmers.py --anti $FASTA 5 > kmers_anti.out
 python3 genotype.py 1e6 6 > genotype.out
 python3 exons.py $DB > exons.out
 python3 params.py $JSON > params.out
