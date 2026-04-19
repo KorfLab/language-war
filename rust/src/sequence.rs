be937d07e0ab4333b9d405aa29359f05e57e9104
shanoaice <25174488+shanoaice@users.noreply.github.com>
2026-04-19 00:07:55 -0700

    feat(rust): sequence entropy

--- rust/src/sequence.rs
+++ rust/src/sequence.rs
@@ -21,3 +21,19 @@ pub fn complement_base(base: char) -> char {
 pub fn reverse_complement(sequence: &str) -> String {
     sequence.chars().rev().map(complement_base).collect()
 }
+
+pub fn entropy(a: isize, c: isize, g: isize, t: isize) -> f64 {
+    let total = (a + c + g + t) as f64;
+    if total == 0.0 {
+        return 0.0;
+    }
+
+    let mut ent = 0.0;
+    for &count in &[a, c, g, t] {
+        if count > 0 {
+            let p = count as f64 / total;
+            ent -= p * p.log2();
+        }
+    }
+    ent
+}
